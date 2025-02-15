fa54c29d0c012ce0de932ee7127de7d81b7fa9ce
LT <15905158132@163.com>
2025-02-15 20:37:00 +0800

    尝试添加软骨系统

--- Engine/Gmm/Animation/StackedSoftElement.cpp
+++ Engine/Gmm/Animation/StackedSoftElement.cpp
@@ -14,24 +14,53 @@
 
 using namespace GM;
 
+StackedSoftElement::StackedSoftElement()
+{
+    Init();
+}
+
+StackedSoftElement::StackedSoftElement(const StackedSoftElement& rhs, const osg::CopyOp& co)
+    : StackedTranslateElement(rhs, co)
+{
+    Init();
+}
+
 StackedSoftElement::StackedSoftElement(const std::string& name, const osg::Vec3& translate)
-    : osgAnimation::StackedTranslateElement(name,translate) {}
+    : osgAnimation::StackedTranslateElement(name, translate)
+{
+    Init();
+}
 
 StackedSoftElement::StackedSoftElement(const osg::Vec3& translate)
     : osgAnimation::StackedTranslateElement(translate)
 {
-    osgAnimation::StackedTranslateElement::setName("translate");
+    setName("translate");
+    Init();
 }
 
-StackedSoftElement::StackedSoftElement() {}
-StackedSoftElement::StackedSoftElement(const StackedSoftElement& rhs, const osg::CopyOp& co)
-    : StackedTranslateElement(rhs, co)
+void StackedSoftElement::Init()
 {
+    if (_bInit) return;
+
+    // 以后会根据每个骨骼的特殊标志生成不同的随机种子
+    m_iRandom.seed(10000 * std::abs(_translate.x() + _translate.y() + _translate.z()));
+    std::uniform_int_distribution<> iPseudoNoise(0, 9999);
+    // 生成随机相位
+    _vSoftPhase = osg::Vec3(
+        iPseudoNoise(m_iRandom) * 1e-3f,
+        iPseudoNoise(m_iRandom) * 1e-3f,
+        iPseudoNoise(m_iRandom) * 1e-3f);
+
+    _bInit = true;
 }
 
 void StackedSoftElement::update(float t)
 {
     StackedTranslateElement::update(t);
 
-    //_translate += _vSoftTranslate * (std::sin(t*10) * 0.5 + 0.5);
+    osg::Vec3 _vSoftOffset = osg::Vec3(
+        _vSoftLimit.x() * (std::sin(t * 10 + _vSoftPhase.x()) * 0.5 + 0.5),
+        _vSoftLimit.y() * std::sin(t * 19.5 + _vSoftPhase.y()),
+        _vSoftLimit.z() * std::sin(t * 19.1 + _vSoftPhase.z()));
+    //_translate += _vSoftOffset;
 }
--- Engine/Gmm/Animation/StackedSoftElement.h
+++ Engine/Gmm/Animation/StackedSoftElement.h
@@ -11,6 +11,7 @@
 //////////////////////////////////////////////////////////////////////////
 #pragma once
 
+#include <random>
 #include <osgAnimation/StackedTranslateElement>
 
 namespace GM
@@ -23,9 +24,13 @@ namespace GM
         StackedSoftElement(const std::string&, const osg::Vec3& translate = osg::Vec3(0,0,0));
         StackedSoftElement(const osg::Vec3& translate);
 
+        void Init();
         void update(float t = 0.0);
 
     protected:
-        osg::Vec3 _vSoftTranslate = osg::Vec3(-0.1f, 0.0f, 0.0f);
+        bool    _bInit = false;
+        std::default_random_engine  m_iRandom;						//!< Ëæ»úÖµ
+        osg::Vec3 _vSoftLimit = osg::Vec3(-0.01f, 0.002f, 0.002f);
+        osg::Vec3 _vSoftPhase = osg::Vec3(0.0f, 0.0f, 0.0f);
     };
 } // namespace GM

98d388842efd13de337a2f4699902be6f4b3b387
LT <15905158132@163.com>
2025-10-25 22:26:13 +0800

    添加加密的工程

---
+++ Encrypt/Encrypt.cpp
@@ -0,0 +1,16 @@
+﻿#include "Cipher/HydroCipher.h"
+#include <iostream>
+
+int main()
+{
+	auto cipher = HydroCipher::create(AlgorithmType::XOR);
+	std::vector<uint8_t> key(32, 0xA8);
+	std::string strSrcFilePath = "../../Data/Core/Models/MIGI.FBX";
+	std::string strDstFilePath = "../../Data/Core/Models/MIGI.CIP";
+	if (cipher->encrypt(strSrcFilePath, strDstFilePath, key))
+		std::cout << "Encryption successful!" << std::endl;
+	else
+		std::cout << "Encryption failed!" << std::endl;
+
+	return 0;
+}
